ae8ee010e66c82a13d395799d445b7cda3093eb4
ikalco <davidmontanolanza@gmail.com>
2020-12-14 16:44:05 -0600

    Fixing conveyor/mat bug

--- Classes/Materials/allMats.js
+++ Classes/Materials/allMats.js
@@ -78,17 +78,16 @@ class GoldPlate extends Material {
 //Trin's
 class IronTrin extends Material {
   constructor(arrX, arrY) {
-    this.xOffSet = (width/gameBoardSize)/4;
-    arrX += this.xOffSet;
     super(arrX, arrY);
     this.item = 'ironTrin'
+    this.xOffSet = (width/gameBoardSize)/4;
   }
 
   
   draw() {
     push();
     fill(255);
-    triangle(this.x + this.w/2, this.y, this.x, this.y + this.h, this.x + this.w, this.y + this.h);
+    triangle((this.x + this.w/2) + this.xOffSet, this.y, (this.x) + this.xOffSet, this.y + this.h, (this.x + this.w) + this.xOffSet, this.y + this.h);
     pop();
   }
   
@@ -99,16 +98,15 @@ class IronTrin extends Material {
 
 class GoldTrin extends Material {
   constructor(arrX, arrY) {
-    this.xOffSet = (width/gameBoardSize)/4;
-    arrX += this.xOffSet;
     super(arrX, arrY);
     this.item = 'ironTrin'
+    this.xOffSet = (width/gameBoardSize)/4;
   }
   
   draw() {
     push();
     fill(255,223,0);
-    triangle(this.x + this.w/2, this.y, this.x, this.y + this.h, this.x + this.w, this.y + this.h);
+    triangle((this.x + this.w/2) + this.xOffSet, this.y, (this.x) + this.xOffSet, this.y + this.h, (this.x + this.w) + this.xOffSet, this.y + this.h);
     pop();
   }
   
