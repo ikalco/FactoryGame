ecf9fb3d644447a50950bffd258ed4362c23977c
ikalco <davidmontanolanza@gmail.com>
2020-12-14 16:47:23 -0600

    Fixing conveyor/mat bug

--- Classes/Materials/allMats.js
+++ Classes/Materials/allMats.js
@@ -80,14 +80,13 @@ class IronTrin extends Material {
   constructor(arrX, arrY) {
     super(arrX, arrY);
     this.item = 'ironTrin'
-    this.xOffSet = (width/gameBoardSize)/4;
   }
 
   
   draw() {
     push();
     fill(255);
-    triangle((this.x + this.w/2) + this.xOffSet, this.y, (this.x) + this.xOffSet, this.y + this.h, (this.x + this.w) + this.xOffSet, this.y + this.h);
+    triangle((this.x + this.w/2) + this.blockW, this.y, (this.x) + this.blockW, this.y + this.h, (this.x + this.w) + this.blockW, this.y + this.h);
     pop();
   }
   
